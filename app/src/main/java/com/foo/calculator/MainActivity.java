eb2aa526a3f91ced1143b0e39a286c14bce9dde8
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-03-12 18:18:55 +0900

    소수점 기능은 예외처리 많은 관계로 생략. 나중에 구현

--- app/src/main/java/com/foo/calculator/MainActivity.java
+++ app/src/main/java/com/foo/calculator/MainActivity.java
@@ -13,8 +13,9 @@ import java.math.RoundingMode;
 public class MainActivity extends AppCompatActivity  {
     private EditText resultValue;
     private String number="";
-    int enterCnt = 0;
-    int option;
+    private int enterCnt = 0;
+    private int option;
+    private int operator;
     public static final int PLUS = 0;
     public static final int MINUS = 1;
     public static final int MULTI = 2;
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         resultValue = findViewById(R.id.result_value);
+        option = DEFAULT;
+        operator = DEFAULT;
+        enterCnt = 0;
     }
 
     public void function(View view){
@@ -35,40 +39,94 @@ public class MainActivity extends AppCompatActivity  {
             case R.id.remove_all:
                 number = "";
                 resultValue.setText("0");
+                operator = DEFAULT;
+                option = DEFAULT;
                 enterCnt = 0;
                 break;
             case R.id.plus_minus:
                 number = resultValue.getText().toString();
-                resultValue.setText(""+Double.parseDouble(number)*-1);
+                if(Double.parseDouble(number)-(int)(Double.parseDouble(number)) == 0.0){
+                    resultValue.setText(""+Integer.parseInt(number)*-1);
+                } else {
+                    resultValue.setText("" + Double.parseDouble(number) * -1);
+                }
                 break;
             case R.id.percent100:
                 number = resultValue.getText().toString();
                 BigDecimal num = new BigDecimal(number);
                 BigDecimal number100 = new BigDecimal(100);
-                double temp = Double.parseDouble(String.valueOf(num.divide(number100)));
-                if(resultValue.length()<=12){
+                if(resultValue.length()<=10){
                     resultValue.setText(""+num.divide(number100));
                 } else{
+                    double temp = Double.parseDouble(String.valueOf(num.divide(number100)));
                     resultValue.setText(""+temp);
                 }
                 option = PERCENT;
+                break;
+           
+            case R.id.plus:
+                number = resultValue.getText().toString();
+                resultValue.setText("");
+                operator = PLUS;
+                break;
+            case R.id.minus:
+                number = resultValue.getText().toString();
+                resultValue.setText("");
+                operator = MINUS;
+                break;
+            case R.id.multiple:
+                number = resultValue.getText().toString();
+                resultValue.setText("");
+                operator = MULTI;
+                break;
+            case R.id.divide:
+                number = resultValue.getText().toString();
+                resultValue.setText("");
+                operator = DIVIDE;
+                break;
+            case R.id.equal:
+                Double ans = null;
+                switch(operator){
+                    case PLUS:
+                        ans = Double.parseDouble(number)+Double.parseDouble(resultValue.getText().toString());
+                        break;
+                    case MINUS:
+                        ans = Double.parseDouble(number)-Double.parseDouble(resultValue.getText().toString());
+                        break;
+                    case MULTI:
+                        ans = Double.parseDouble(number)*Double.parseDouble(resultValue.getText().toString());
+                        break;
+                    case DIVIDE:
+                        ans = Double.parseDouble(number)/Double.parseDouble(resultValue.getText().toString());
+                        if(ans.toString().length()>=10){
+                            ans = (double)Math.round(ans*10000000)/10000000.0;
+                        }
+                        if(ans.isInfinite() || ans.isNaN()){
+                            number = null;
+                            resultValue.setText("오류");
+                            return;
+                        }
+                        break;
+                }
+                resultValue.setText(""+ans);
+                number = resultValue.getText().toString();
                 break;
         }
     }
 
     public void num(View view){
+
         if(enterCnt == 0){
             resultValue.setText("");
         }
 
-        if(resultValue.getText().toString().contains("E") || option == PERCENT){
+        if(resultValue.getText().toString().contains("E") || option == PERCENT ){
             resultValue.setText("");
             option = DEFAULT;
         }
 
         switch(view.getId()){
             case R.id.one:
-
                 resultValue.setText(resultValue.getText().toString()+"1");
                 break;
             case R.id.two:
